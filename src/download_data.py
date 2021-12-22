2c07e5ea1721fa9449a8167af0f4b67b0d9f185a
Utkarsh Saboo <utkarshsaboo45@gmail.com>
2021-12-21 16:42:52 -0800

    Define Column Types, Do Preprocessing

--- src/download_data.py
+++ src/download_data.py
@@ -4,6 +4,7 @@
 Download the csv file from the given url and save it in the filepath given
 
 Usage: src/download_data.py --url=<url> --filepath=<filepath>
+Example: python src/download_data.py --url=http://data.insideairbnb.com/united-states/ny/new-york-city/2021-11-02/visualisations/listings.csv --filepath=data/raw/airbnb.csv
 
 Options:
 --url=<url>                 URL of the csv file
@@ -15,18 +16,16 @@ from docopt import docopt
 import pandas as pd
 import os
 
-opt = docopt(__doc__)
-
 def main(url, filepath):
-    
+
     data = pd.read_csv(url, header=None)
 
     if not os.path.exists(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
 
-    data.to_csv(filepath, encoding="utf-8")
+    data.to_csv(filepath, encoding="utf-8", index=False, header=False)
 
-if __name__=="__main__":
+
+if __name__== "__main__":
+    opt = docopt(__doc__)
     main(opt["--url"], opt["--filepath"])
-    
-    
